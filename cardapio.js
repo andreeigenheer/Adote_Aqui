ff3aa44023bd63b5ce568af64a1f66178da55c5b
andreeigenheer <eigenheer81@gmail.com>
2021-04-06 18:16:59 -0300

    primeira versao

---
+++ cardapio.js
@@ -0,0 +1,21 @@
+var atum = 20;
+var calabresa = 22.50;
+var portuguesa = 34;
+var mussarela = 20;
+var frangoEcatupiry = 35;
+var marguerita = 20; 
+
+function cardapio () { 
+    alert ("Boa noite! Temos os sabores disponíveis: Atum R$20.00. Calabresa R$22.50. Portuguesa R$ 34.00. Mussarela R$20.00. Frango e catupiry R$20.00 e Marguerita R$20.00");
+    var opcao = prompt ("Qual sabor da pizza gostaria?");
+    if (opcao = atum) { 
+    return prompt ("Você precisa de troco?");
+
+     }
+ 
+
+
+}
+cardapio (); 
+
+function troco ()
